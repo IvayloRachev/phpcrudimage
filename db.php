f3185cdf547b0cbcb1c983ee855eafac94347cff
IvayloRachev <82808251+IvayloRachev@users.noreply.github.com>
2021-07-28 10:35:24 +0200

    connexion a la base de donnees

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+$dsn = "mysql:host=localhost;dbname=cinema";
+$username = "root";
+$password = "";
+$options = [];
+$connection = new PDO($dsn, $username, $password, $options);
+try {
+
+    echo "connexion reussie";
+} catch(PDOException $e){
+    print "error :" . $e->getMessage();
+    die();
+}

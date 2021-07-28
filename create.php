2fd6080c1f77bab93399000eaeaa0359db59477e
IvayloRachev <82808251+IvayloRachev@users.noreply.github.com>
2021-07-28 13:41:41 +0200

    modif page create

--- create.php
+++ create.php
@@ -1,5 +1,28 @@
 <?php
 require './db.php';
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+if(
+    isset($_POST['titre']) &&
+    isset($_POST['annee']) &&
+    isset($_POST['image'])
+    ){
+        $titre = $_POST['titre'];
+        $annee = $_POST['annee'];
+        $image = $_POST['image'];
+        $sql = "INSERT INTO film(titre, annee, image) VALUES (:titre, :annee, :image)";
+        $statement = $connection->prepare($sql);
+        if($statement->execute(
+            [
+                ':titre' => $titre,
+                ':annee' => $annee,
+                ':image' => $image
+                ]
+        )){
+           print "Ajouté avec succes";
+        }
+}
 require './header.php';
 ?>
 
@@ -7,14 +30,14 @@ require './header.php';
     <div class="row">
         <div class="col">
             <h1>Ajouter un film</h1>
-            <form>
+            <form method="post">
                 <div class="form-group">
                     <label>Titre</label>
                     <input type="text" class="form-control" name="titre">
                 </div>
                 <div class="form-group">
                     <label>Date</label>
-                    <input type="date" class="form-control" name="date">
+                    <input type="date" class="form-control" name="annee">
                 </div>
                 <div class="form-group">
                     <label>Image</label>

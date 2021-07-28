6b2d9ddda7588ce96c7bc7045d4a2a32366d0a0b
IvayloRachev <82808251+IvayloRachev@users.noreply.github.com>
2021-07-28 11:47:07 +0200

    affichage page index

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
+require './db.php';
+$sql = "SELECT * FROM film;";
+$statement = $connection->prepare($sql);
+$statement->execute();
+$films = $statement->fetchAll(PDO::FETCH_OBJ);
+?>
+
+<?php
 require './header.php';
 ?>
 <div class="container">
@@ -17,16 +25,18 @@ require './header.php';
                     </tr>
                 </thead>
                 <tbody>
+                    <?php foreach($films as $element): ?>
                     <tr>
-                        <th scope="row">1</th>
-                        <td>cobra</td>
-                        <td>1993</td>
-                        <td><img src="./images/cobra.jpg" width="200"></td>
+                        <th scope="row"><?= $element->id ?></th>
+                        <td><?= $element->titre ?></td>
+                        <td><?= $element->annee ?></td>
+                        <td><img src="<?= $element->image ?>" width="200"></td>
                         <td>
                             <button type="button" class="btn btn-success">Editer</button>
                             <button type="button" class="btn btn-danger">Supprimer</button>
                         </td>
                     </tr>
+                    <?php endforeach; ?>
                 </tbody>
             </table>
 
